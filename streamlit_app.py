f921a55a10b5c82e0e500e983e224578e3469d4c
swarnali162347 <138047059+swarnali162347@users.noreply.github.com>
2023-07-17 23:23:59 +0530

    Update streamlit_app.py
    remove json unformatted data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.dataframe (fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 #normalize the json version
 
