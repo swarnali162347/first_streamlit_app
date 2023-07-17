6aa7d60043fb120487c243332b720ac844a19e2e
swarnali162347 <138047059+swarnali162347@users.noreply.github.com>
2023-07-18 01:06:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,30 +26,37 @@ streamlit.dataframe (fruits_to_show)
 
 #New section to display fruityvice api response
 
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+ #   streamlit.error("Please select a fruit to get information.")
+ # else:
+ #   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ #   streamlit.dataframe(fruityvice_normalized)
+#streamlit.write('The user entered ', fruit_choice)
+
+#except URLError as e:
+  #streamlit.error()
+
+#create function
+#below section from ln 45 to 59 re-written to replace ln 29-41
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+#new section to display fruityvice api response
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-#streamlit.write('The user entered ', fruit_choice)
-
-except URLError as e:
-  streamlit.error()
-
-#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-
-#normalize the json version
-
-
-
-# Display the table on the page.
-
-
-
+   else:
+     back_from_function = get_fruityvice_data(fruit_choice)
+     streamlit.dataframe(back_from_function)
 
 streamlit.stop()
 
